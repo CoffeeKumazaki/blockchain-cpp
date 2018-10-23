45fe6baf8c3fba8fb752f47b6a69e3538cb9b1ea
merakm <kumazaki98+github@gmail.com>
2018-10-24 01:38:45 +0900

    test blockchain

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+#include "blockchain.h"
+
+int main(int argc, char** argv) {
+
+    CBlockchain  chain;
+
+    Transaction trans1;
+    trans1.amount = 100;
+    trans1.senderKey = "John";
+    trans1.receiverKey = "Bob";
+    trans1.timestamp = time(NULL);
+    chain.addBlock(trans1);
+
+
+    Transaction trans2;
+    trans2.amount = 200;
+    trans2.senderKey = "Alice";
+    trans2.receiverKey = "Lisa";
+    trans2.timestamp = time(NULL);
+    chain.addBlock(trans2);
+
+    chain.print();
+
+}
